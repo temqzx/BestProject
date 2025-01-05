69a76a954fb789841e685dd0f31dc42929973dcd
temqzx <dojnikovdenis710@gmail.com>
2025-01-05 19:55:59 +0300

    BestProject

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ menu.addEventListener('click', function(event){
    if (clickTarget.classList.contains('nav-link')){
        clickTarget.classList.add('active');
        activeBtn.classList.remove('active');
+
+      let mobile = document.querySelector('.mobile-menu');
+      if (!mobile.classList.contains('hide')){
+        mobile.classList.add('hide');
+      }
+      let body = document.querySelector("body");
+      let mobileMenu = document.querySelector(".mobile-menu");
+      if (!body.classList.contains("off-scroll") &&
+                  !mobileMenu.classList.contains("hide")){
+          body.classList.add("off-scroll");
+      }
+      else{
+        body.classList.remove("off-scroll");
+      }
    }
 });
 
@@ -36,3 +50,10 @@ window.onscroll = function() {
    activeBtn.classList.remove('active')
  }
 };
+
+document.querySelector(".mobile-button").addEventListener("click",
+function(event)
+{
+   document.querySelector(".mobile-menu").classList.toggle("hide");
+});
+
